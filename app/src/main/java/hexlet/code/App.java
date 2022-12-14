a2dcdacdb908871073da1fb2815e1e85c15171c2
Lilia <ifuxhe@gmail.com>
2022-12-14 17:40:10 +0100

    fixed Greeting

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Brain Games!\nPlease enter the game number and press Enter.");
+        System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
@@ -21,7 +21,7 @@ public class App {
         String choice = scanner.nextLine();
 
         if (choice.equals("1")) {
-            Cli.greeting();
+            Greet.greeting();
         } else if (choice.equals("2")) {
             Even.playGame();
         } else if (choice.equals("3")) {
--- app/src/main/java/hexlet/code/Greet.java
+++ app/src/main/java/hexlet/code/Greet.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 public class Greet {
     public static String username;
     public static void greeting() {
-        System.out.println("Welcome to the Brain Games!");
-
         Scanner myObj = new Scanner(System.in);
-        System.out.println("May I have your name?");
+
+        System.out.println("Welcome to the Brain Games!\nMay I have your name?");
         username = myObj.nextLine();
         System.out.println("Hello, " + username + "!");
     }
