77d054cbc4162f64ee6730d1ae81a30977e3be84
Lilia <ifuxhe@gmail.com>
2022-12-14 17:21:56 +0100

    added welcome line

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("Welcome to the Brain Games!\nPlease enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
