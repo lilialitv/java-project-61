7b32ebd28998e4a04684843f6f059ffd827213e6
Lilia <ifuxhe@gmail.com>
2023-01-27 13:11:20 +0100

    greeting fixed

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting() {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
         String name = sc.next();
         System.out.println("Hello, " + name + "!");
